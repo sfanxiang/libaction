b88b8ac0ef90ee038173cdd272abeabfcb3d3800
Xiang Fan <sfanxiang@gmail.com>
2018-05-17 16:05:40 +0800

    estimator: tmp

--- include/libaction/estimator.hpp
+++ include/libaction/estimator.hpp
@@ -7,6 +7,7 @@
 #include <tensorflow/contrib/lite/kernels/register.h>
 #include <tensorflow/contrib/lite/model.h>
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
 #include <memory>
 #include <stdexcept>
@@ -112,25 +113,42 @@ public:
 			}
 		}
 
-		std::vector<std::pair<size_t, size_t>> coords;
+		std::vector<std::unique_ptr<std::vector<std::pair<size_t, size_t>>>>
+			coords;
 		for (size_t i = 0; i < heat_mat_shape[0] - 1; i++) {
 			auto s1 = array::suppress_threshold<Value>(&heat_mat[array::index(3, heat_mat_shape, {i, 0, 0})],
 				heat_mat_shape[1], heat_mat_shape[2], nms_threshold);
 			auto s2 = array::suppress_non_max<Value>(s1.get(), heat_mat_shape[1], heat_mat_shape[2], nms_window, nms_window);
 			auto coord = array::where_not_less(s2.get(), heat_mat_shape[1], heat_mat_shape[2], nms_threshold);
-			for (auto &c: *coord) {
-				coords.push_back(c);
-			}
+			coords.push_back(std::move(coord));
 		}
-		for (auto &c: coords) {
-			std::cout << c.first << ' ' << c.second << std::endl;
-		}
+
+
 	}
 
 private:
-	const Value nms_threshold = 0.15;
-	const size_t nms_window = 5;
+	struct PartPair
+	{
+		float score;
+		size_t part_idx1, part_idx2;
+		size_t idx1, idx2;
+		size_t coord1, coord2;
+		float score1, score2;
+
+		PartPair(float sc, size_t pi1, size_t pi2, size_t i1, size_t i2,
+			size_t cd1, size_t cd2, float sc1, float sc2)
+		:
+			score(sc), part_idx1(pi1), part_idx2(pi2), idx1(i1), idx2(i2),
+			coord1(cd1), coord2(cd2), score1(sc1), score2(sc2)
+		{}
+	};
+
 	const size_t heat_mat_shape_0 = 19, paf_mat_shape_0 = 38;
+	const float nms_threshold = 0.15;
+	const size_t nms_window = 5;
+	const size_t paf_num_inter = 10;
+	const float local_paf_threshold = 0.2;
+	const size_t paf_count_threshold = 5;
 
 	size_t model_height, model_width, model_channels;
 
@@ -147,6 +165,103 @@ private:
 	inline Value *get_output()
 	{
 		return interpreter->typed_output_tensor<Value>(0);
+	}
+
+	std::unique_ptr<std::vector<PartPair>> score_pairs(
+		size_t part_idx1, size_t part_idx2,
+		const std::vector<std::pair<size_t, size_t>> &coord_list1,
+		const std::vector<std::pair<size_t, size_t>> &coord_list2,
+		Value *paf_mat_x, Value *paf_mat_y, Value *heatmap,
+		float rescale1, float rescale2)
+	{
+		size_t idx1 = 0;
+		for (auto coord1 = coord_list1.begin();
+				coord1 != coord_list1.end();
+				++coord1, ++idx1) {
+			auto y1 = coord1->first;
+			auto x1 = coord1->second;
+
+			size_t idx2 = 0;
+			for (auto coord2 = coord_list2.begin();
+					coord2 != coord_list2.end();
+					++coord2, ++idx2) {
+				auto y2 = coord2->first;
+				auto x2 = coord2->second;
+
+				auto score_count = get_score(x1, y1, x2, y2, paf_mat_x, paf_mat_y);
+				auto &score = score_count.first;
+				auto &count = score_count.second;
+
+				if (count < paf_count_threshold || score <= 0.0f)
+					continue;
+
+				// TODO
+			}
+		}
+	}
+
+	/* TODO: REMOVE:
+
+    def score_pairs(part_idx1, part_idx2, coord_list1, coord_list2, paf_mat_x, paf_mat_y, heatmap, rescale=(1.0, 1.0)):
+        connection_temp = []
+
+        for idx1, (y1, x1) in enumerate(zip(coord_list1[0], coord_list1[1])):
+            for idx2, (y2, x2) in enumerate(zip(coord_list2[0], coord_list2[1])):
+                score, count = PoseEstimator.get_score(x1, y1, x2, y2, paf_mat_x, paf_mat_y)
+                if count < PoseEstimator.PAF_Count_Threshold or score <= 0.0:
+                    continue
+                connection_temp.append(PoseEstimator.PartPair(
+                    score=score,
+                    part_idx1=part_idx1, part_idx2=part_idx2,
+                    idx1=idx1, idx2=idx2,
+                    coord1=(x1 * rescale[0], y1 * rescale[1]),
+                    coord2=(x2 * rescale[0], y2 * rescale[1]),
+                    score1=heatmap[part_idx1][y1][x1],
+                    score2=heatmap[part_idx2][y2][x2],
+                ))
+
+        connection = []
+        used_idx1, used_idx2 = set(), set()
+        for candidate in sorted(connection_temp, key=lambda x: x.score, reverse=True):
+            # check not connected
+            if candidate.idx1 in used_idx1 or candidate.idx2 in used_idx2:
+                continue
+            connection.append(candidate)
+            used_idx1.add(candidate.idx1)
+            used_idx2.add(candidate.idx2)
+
+        return connection
+
+	*/
+
+	std::pair<float, size_t> get_score(
+		size_t x1, size_t y1, size_t x2, size_t y2,
+		Value *paf_mat_x, Value *paf_mat_y)
+	{
+		auto dx = x2 - x1;
+		auto dy = y2 - y1;
+		auto norm_vec = hypotf(static_cast<float>(dx), static_cast<float>(dy));
+
+		if (norm_vec < 1e-4f)
+			return std::make_pair(0.0f, 0);
+
+		float vx = static_cast<float>(dx) / norm_vec;
+		float vy = static_cast<float>(dy) / norm_vec;
+
+		float score = 0.0f;
+		size_t count = 0;
+		for (size_t i = 0; i < paf_num_inter; i++) {
+			size_t x = x1 + (dx * i + paf_num_inter / 2) / paf_num_inter;
+			size_t y = y1 + (dy * i + paf_num_inter / 2) / paf_num_inter;
+
+			float curr = paf_mat_x[y][x] * vx + paf_mat_y[y][x] * vy;
+			if (curr > local_paf_threshold) {
+				score += curr;
+				count++;
+			}
+		}
+
+		return std::make_pair(score, count);
 	}
 };
 
