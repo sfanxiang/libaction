b6be3c1fbedadc54538ce35b3c10377ec5d59f7c
Xiang Fan <sfanxiang@gmail.com>
2018-06-16 11:03:06 +0800

    update motion estimator

--- include/libaction/motion/single/estimator.hpp
+++ include/libaction/motion/single/estimator.hpp
@@ -8,12 +8,14 @@
 #ifndef LIBACTION__MOTION__SINGLE__ESTIMATOR_HPP_
 #define LIBACTION__MOTION__SINGLE__ESTIMATOR_HPP_
 
+#include "libaction/body_part.hpp"
 #include "libaction/human.hpp"
 
 #include <cstdint>
 #include <functional>
 #include <memory>
 #include <unordered_map>
+#include <utility>
 
 namespace libaction
 {
@@ -22,17 +24,42 @@ namespace motion
 namespace single
 {
 
+/// Single-person motion estimator.
 class Estimator
 {
 public:
+	/// Constructor.
 	inline Estimator();
 
 	template<typename StillEstimator, typename ImagePtr>
 	inline std::unique_ptr<std::unordered_map<size_t, libaction::Human>>
 	estimate(
-		size_t pos, size_t length, size_t rate, StillEstimator &still_estimator
+		size_t pos, size_t length, size_t rate, size_t fuzz_range,
+		StillEstimator &still_estimator,
 		std::unique_ptr<std::function<ImagePtr(size_t pos)>> callback
 	) {
+		if (length == 0) {
+			throw std::runtime_error("length == 0");
+		}
+
+		auto pose_pos = poses.find(pos);
+		if (pose_pos != poses.end()) {
+			// return existing pose
+			if (*pose_pos) {
+				return std::unique_ptr<std::unordered_map<size_t, libaction::Human>>(
+					new std::unordered_map<size_t, libaction::Human>>(
+						{ 0, **pose_pos }));
+			} else {
+				return std::unique_ptr<std::unordered_map<size_t, libaction::Human>>(
+					new std::unordered_map<size_t, libaction::Human>>());
+			}
+		}
+
+		auto pose_initial = poses.find(0);
+		if (pose_initial == poses.end()) {
+			// the initial frame has not been processed
+			pose_initial = process_initial_frame(still_estimator, callback);
+		}
 	}
 
 	inline void reset()
@@ -40,7 +67,34 @@ public:
 	}
 
 private:
+	std::unordered_map<size_t, std::unique_ptr<libaction::Human>> poses{};
+	std::unordered_set<libaction::BodyPart::PartIndex> avail{};
 
+	/// Process the unprocessed initial frame.
+	void process_initial_frame(StillEstimator &still_estimator,
+		std::unique_ptr<std::function<ImagePtr(size_t pos)>> callback)
+	{
+		auto humans = still_estimator.estimate((*callback)(pos));
+		std::unique_ptr<libaction::Human> human;
+
+		if (!humans->empty()) {
+			human = std::unique_ptr<libaction::Human>(
+				new libaction::Human(std::move(*humans->begin())));
+		}
+
+		auto pose_initial = poses.insert(std::make_pair(0, std::move(human)))
+			.first;
+
+		// body parts present in the initial frame are considered available
+		avail.reset();
+		if (*pose_initial) {
+			for (auto &part: (*pose_initial)->body_parts()) {
+				avail.insert(part.first);
+			}
+		}
+
+		return pose_initial;
+	}
 };
 
 }
