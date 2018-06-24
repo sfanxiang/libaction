12feafd7f744dd90b83bd9ed32b52776ff9006e4
Xiang Fan <sfanxiang@gmail.com>
2018-06-23 21:07:22 -0700

    motion: estimator: refactor and begin multithread support

--- include/libaction/motion/single/estimator.hpp
+++ include/libaction/motion/single/estimator.hpp
@@ -18,10 +18,13 @@
 #include <cstdint>
 #include <functional>
 #include <memory>
+#include <mutex>
+#include <thread>
 #include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
+#include <vector>
 
 namespace libaction
 {
@@ -56,9 +59,10 @@ public:
 	///                         parameter is used.
 	/// @param[in]  zoom_rate   The stride used for zoom reestimation. Must be
 	///                         greater than 0.
-	/// @param[in]  still_estimator An initialized human pose estimator, whose
-	///                             `estimate()` method must accept any image
-	///                             conforming to the Boost.MultiArray concept.
+	/// @param[in]  still_estimators A vector of one or more initialized human
+	///                              pose estimators, whose `estimate()` method
+	///                              must accept any image conforming to the
+	///                              Boost.MultiArray concept.
 	/// @param[in]  callback    A callback function allowing random access to
 	///                         the image frame at `pos`. The callback should
 	///                         return a valid pointer to the image, which must
@@ -73,7 +77,7 @@ public:
 		size_t pos, size_t length,
 		size_t fuzz_range, size_t fuzz_rate,
 		bool zoom, size_t zoom_range, size_t zoom_rate,
-		StillEstimator &still_estimator,
+		const std::vector<StillEstimator&> &still_estimators,
 		const std::function<ImagePtr(size_t pos)> &callback
 	) {
 		if (length == 0)
@@ -84,138 +88,58 @@ public:
 			throw std::runtime_error("fuzz_rate == 0");
 		if (zoom_rate == 0)
 			throw std::runtime_error("zoom_rate == 0");
+		if (still_estimators.empty())
+			throw std::runtime_error("still_estimators is empty");
+
+		if (still_estimators.size() > 1) {
+			// multi-thread support (preprocessing)
+
+			// prioriry order:
+			std::unordered_set<size_t>
+				frames_unzoomed_1, frames_zoomed, frames_unzoomed_2;
+
+			size_t fuzz_l = pos;
+			size_t fuzz_r = pos;
+
+			if (fuzz_range != 0) {
+				if (pos >= fuzz_range - 1)
+					fuzz_l = pos - (fuzz_range - 1);
+				else
+					fuzz_l = 0;
+				if (length - pos > fuzz_range - 1)
+					fuzz_r = pos + (fuzz_range - 1);
+				else
+					fuzz_r = length - 1;
+			}
+
+			for (size_t i = fuzz_l; i <= fuzz_r; i++) {
+				frames_unzoomed_2.insert(i);
+			}
+
+			for (size_t i = fuzz_l; i <= fuzz_r; i++) {
+				if (needs_zoom(i, zoom_rate)) {
+					size_t zoom_l = (pos >= zoom_range ? pos - zoom_range : 0);
+					size_t zoom_r = (length - pos > zoom_range ? pos + zoom_range : length - 1);
+
+					for (size_t j = zoom_l; j <= zoom_r; j++) {
+						frames_unzoomed_2.erase(j);
+						frames_unzoomed_1.insert(j);
+					}
+
+					frames_zoomed.insert(i);
+				}
+			}
+
+			std::mutex mutex;
+			// TODO
+		}
 
 		std::function<std::pair<bool, const libaction::Human *>(size_t, bool)> fuzz_cb
-			= [pos, length, zoom, zoom_range, zoom_rate, &still_estimator, &callback, this]
+			= [pos, length, zoom, zoom_range, zoom_rate, &still_estimators, &callback, this]
 				(size_t offset, bool left) -> std::pair<bool, const libaction::Human *>
 		{
-			size_t pos_ = pos;
-			size_t pos = pos_;
-
-			if (pos >= length)
-				return std::make_pair(false, nullptr);
-
-			// get the real pos
-			if (left) {
-				if (offset > pos)
-					return std::make_pair(false, nullptr);
-				else
-					pos -= offset;
-			} else {
-				if (offset >= length - pos)
-					return std::make_pair(false, nullptr);
-				else
-					pos += offset;
-			}
-
-			{
-				// Does pos already exist in still_poses?
-				auto it = still_poses.find(pos);
-				if (it != still_poses.end())
-					return std::make_pair(true, it->second.get());
-			}
-
-			// pos does not exist in still_poses. We need to estimate it.
-
-			if (zoom && needs_zoom(pos, zoom_rate)) {
-				// the image at pos needs to be zoomed
-
-				ImagePtr image;	// image at pos
-
-				// make sure that pos exists in unzoomed_still_poses
-				auto unzoomed_it = unzoomed_still_poses.find(pos);
-				if (unzoomed_it == unzoomed_still_poses.end()) {
-					std::tie(image, unzoomed_it) =
-						estimate_still_pose_from_callback_on(
-							pos, callback, still_estimator,
-							unzoomed_still_poses
-						);
-				}
-
-				if (unzoomed_it->second) {
-					// human found in the unzoomed image
-
-					// now prepare the hints for a zoomed estimation
-
-					size_t l = (pos >= zoom_range ? pos - zoom_range : 0);
-					size_t r = (length - pos > zoom_range ? pos + zoom_range : length - 1);
-					std::vector<const libaction::Human *> hints;
-
-					// make all unzoomed estimations available within zoom_range
-					for (size_t i = l; i <= r; i++) {
-						if (i == pos)
-							continue;
-
-						decltype(still_poses)::iterator it;
-						if (needs_zoom(i, zoom_rate)) {
-							// unzoomed estimations for images which should be
-							// zoomed go to unzoomed_still_poses
-
-							it = unzoomed_still_poses.find(i);
-							if (it == unzoomed_still_poses.end()) {
-								it = estimate_still_pose_from_callback_on(
-									i, callback, still_estimator,
-									unzoomed_still_poses
-								).second;
-							}
-						} else {
-							// estimations for images which should not be zoomed
-							// go to still_poses
-
-							it = still_poses.find(i);
-							if (it == still_poses.end()) {
-								it = estimate_still_pose_from_callback_on(
-									i, callback, still_estimator,
-									still_poses
-								).second;
-							}
-						}
-
-						if (it->second)	// a useful hint
-							hints.push_back(it->second.get());
-					}
-
-					if (!image) {
-						// image hasn't been retrieved from callback
-						image = get_image_from_callback(pos, callback);
-					}
-
-					// zoom estimate
-					using zoom_cb_arg = boost::multi_array<typename
-						std::remove_reference<decltype(*image)>::type::element,
-						3
-					>;
-					std::function<std::unique_ptr<libaction::Human>
-						(const zoom_cb_arg&)> zoom_cb =
-					[&still_estimator] (const zoom_cb_arg &image_to_estimate) {
-						return estimate_still_pose_from_image(image_to_estimate,
-							still_estimator);
-					};
-					auto human = libaction::still::single::zoom::zoom_estimate(
-						*image, *unzoomed_it->second, hints, zoom_cb);
-
-					// zoomed estimations for images which should be zoomed go
-					// to still_poses
-					auto it = still_poses.insert(
-						std::make_pair(pos, std::move(human))
-					).first;
-
-					return std::make_pair(true, it->second.get());
-				} else {
-					// no human found in unzoomed image
-					// impossible to do zoomed estimation
-					still_poses.insert(std::make_pair(pos, std::unique_ptr<libaction::Human>()));
-
-					return std::make_pair(true, nullptr);
-				}
-			} else {
-				// the image at pos does not need to be zoomed
-
-				auto it = estimate_still_pose_from_callback_on(
-					pos, callback, still_estimator, still_poses).second;
-
-				return std::make_pair(true, it->second.get());
-			}
+			return fuzz_callback(pos, length, zoom, zoom_range, zoom_rate,
+				still_estimators, callback, offset, left);
 		};
 
 		auto human = detail::fuzz::fuzz(fuzz_range, fuzz_rate, fuzz_cb);
@@ -301,6 +225,144 @@ private:
 			still_estimator, poses);
 
 		return std::make_pair(std::move(image), it);
+	}
+
+	template<typename StillEstimator, typename ImagePtr>
+	std::pair<bool, const libaction::Human *>
+	fuzz_callback(size_t pos, size_t length,
+		bool zoom, size_t zoom_range, size_t zoom_rate,
+		std::vector<StillEstimator&> &still_estimators,
+		const std::function<ImagePtr(size_t pos)> &callback,
+		size_t offset, bool left)
+	{
+		auto &still_estimator = *still_estimators.begin();
+
+		if (pos >= length)
+			return std::make_pair(false, nullptr);
+
+		// get the real pos
+		if (left) {
+			if (offset > pos)
+				return std::make_pair(false, nullptr);
+			else
+				pos -= offset;
+		} else {
+			if (offset >= length - pos)
+				return std::make_pair(false, nullptr);
+			else
+				pos += offset;
+		}
+
+		{
+			// Does pos already exist in still_poses?
+			auto it = still_poses.find(pos);
+			if (it != still_poses.end())
+				return std::make_pair(true, it->second.get());
+		}
+
+		// single-thread support
+
+		// pos does not exist in still_poses. We need to estimate it.
+
+		if (zoom && needs_zoom(pos, zoom_rate)) {
+			// the image at pos needs to be zoomed
+
+			ImagePtr image;	// image at pos
+
+			// make sure that pos exists in unzoomed_still_poses
+			auto unzoomed_it = unzoomed_still_poses.find(pos);
+			if (unzoomed_it == unzoomed_still_poses.end()) {
+				std::tie(image, unzoomed_it) =
+					estimate_still_pose_from_callback_on(
+						pos, callback, still_estimator,
+						unzoomed_still_poses
+					);
+			}
+
+			if (unzoomed_it->second) {
+				// human found in the unzoomed image
+
+				// now prepare the hints for a zoomed estimation
+
+				size_t l = (pos >= zoom_range ? pos - zoom_range : 0);
+				size_t r = (length - pos > zoom_range ? pos + zoom_range : length - 1);
+				std::vector<const libaction::Human *> hints;
+
+				// make all unzoomed estimations available within zoom_range
+				for (size_t i = l; i <= r; i++) {
+					if (i == pos)
+						continue;
+
+					decltype(still_poses)::iterator it;
+					if (needs_zoom(i, zoom_rate)) {
+						// unzoomed estimations for images which should be
+						// zoomed go to unzoomed_still_poses
+
+						it = unzoomed_still_poses.find(i);
+						if (it == unzoomed_still_poses.end()) {
+							it = estimate_still_pose_from_callback_on(
+								i, callback, still_estimator,
+								unzoomed_still_poses
+							).second;
+						}
+					} else {
+						// estimations for images which should not be zoomed
+						// go to still_poses
+
+						it = still_poses.find(i);
+						if (it == still_poses.end()) {
+							it = estimate_still_pose_from_callback_on(
+								i, callback, still_estimator,
+								still_poses
+							).second;
+						}
+					}
+
+					if (it->second)	// a useful hint
+						hints.push_back(it->second.get());
+				}
+
+				if (!image) {
+					// image hasn't been retrieved from callback
+					image = get_image_from_callback(pos, callback);
+				}
+
+				// zoom estimate
+				using zoom_cb_arg = boost::multi_array<typename
+					std::remove_reference<decltype(*image)>::type::element,
+					3
+				>;
+				std::function<std::unique_ptr<libaction::Human>
+					(const zoom_cb_arg&)> zoom_cb =
+				[&still_estimator] (const zoom_cb_arg &image_to_estimate) {
+					return estimate_still_pose_from_image(image_to_estimate,
+						still_estimator);
+				};
+				auto human = libaction::still::single::zoom::zoom_estimate(
+					*image, *unzoomed_it->second, hints, zoom_cb);
+
+				// zoomed estimations for images which should be zoomed go
+				// to still_poses
+				auto it = still_poses.insert(
+					std::make_pair(pos, std::move(human))
+				).first;
+
+				return std::make_pair(true, it->second.get());
+			} else {
+				// no human found in unzoomed image
+				// impossible to do zoomed estimation
+				still_poses.insert(std::make_pair(pos, std::unique_ptr<libaction::Human>()));
+
+				return std::make_pair(true, nullptr);
+			}
+		} else {
+			// the image at pos does not need to be zoomed
+
+			auto it = estimate_still_pose_from_callback_on(
+				pos, callback, still_estimator, still_poses).second;
+
+			return std::make_pair(true, it->second.get());
+		}
 	}
 
 	/// Get the processed human pose to return to the user.
